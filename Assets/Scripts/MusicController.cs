9b9d6299db3fba0dfb2b9d4e4e483093a59c08b3
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-06-06 23:45:34 -0700

    Forgot one thing. now project is complete
    music stuff

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -18,7 +18,7 @@ public class MusicController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentlyPlaying.clip = dayPeace;
+        currentlyPlaying.clip = dayBattle;
     }
 
     public IEnumerator transition()
